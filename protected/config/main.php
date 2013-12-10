c483b44ed23aca478f253e51ac0021271514348d
vetalt <vitaly.tarasov@gmail.com>
2013-12-10 03:01:14 +0200

    skeleton
    yii + yii-booster

---
+++ protected/config/main.php
@@ -0,0 +1,83 @@
+<?php
+
+// uncomment the following to define a path alias
+Yii::setPathOfAlias('vendor', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+    'theme' => 'bootstrap',
+    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+    'name' => 'Trask',
+    // preloading 'log' component
+    'preload' => array('log', 'bootstrap'),
+    // autoloading model and component classes
+    'import' => array(
+        'application.models.*',
+        'application.components.*',
+    ),
+    'modules' => array(
+        'gii' => array(
+            'generatorPaths' => array(
+                'bootstrap.gii',
+            ),
+            'class' => 'system.gii.GiiModule',
+            'password' => 'trask',
+            // If removed, Gii defaults to localhost only. Edit carefully to taste.
+            'ipFilters' => array('127.0.0.1', '::1'),
+        ),
+    ),
+    // application components
+    'components' => array(
+        'bootstrap' => array(
+            'class' => 'vendor.clevertech.yii-booster.src.components.Bootstrap'
+        ),
+        'user' => array(
+            // enable cookie-based authentication
+            'allowAutoLogin' => true,
+        ),
+        // uncomment the following to enable URLs in path-format
+        /*
+          'urlManager'=>array(
+          'urlFormat'=>'path',
+          'rules'=>array(
+          '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+          '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+          '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+          ),
+          ),
+         */
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;dbname=trask',
+            'emulatePrepare' => true,
+            'username' => 'trask',
+            'password' => 'trask',
+            'charset' => 'utf8',
+        ),
+        'errorHandler' => array(
+            // use 'site/error' action to display errors
+            'errorAction' => 'site/error',
+        ),
+        'log' => array(
+            'class' => 'CLogRouter',
+            'routes' => array(
+                array(
+                    'class' => 'CFileLogRoute',
+                    'levels' => 'error, warning',
+                ),
+            // uncomment the following to show log messages on web pages
+            /*
+              array(
+              'class'=>'CWebLogRoute',
+              ),
+             */
+            ),
+        ),
+    ),
+    // application-level parameters that can be accessed
+    // using Yii::app()->params['paramName']
+    'params' => array(
+        // this is used in contact page
+        'adminEmail' => 'webmaster@example.com',
+    ),
+);
